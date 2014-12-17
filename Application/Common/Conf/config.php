d60207c1b381cb507eb29054b1f02c80591c1b0f
a <1052746142@qq.com>
2014-12-17 11:00:13 +0800

    asdd

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+	'DEFAULT_MODULE'     => 'Index', //默认模块
+    'URL_MODEL'          => '2', //URL模式
+    'SESSION_AUTO_START' => true, //是否开启session
+
+
+	//数据库配置信息
+	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_HOST'   => '192.168.1.95', // 服务器地址
+	'DB_NAME'   => 'mybbs', // 数据库名
+	'DB_USER'   => 'root', // 用户名
+	'DB_PWD'    => 'root', // 密码
+	'DB_PORT'   => 3306, // 端口
+	'DB_PREFIX' => 'think_', // 数据库表前缀 
+	'DB_CHARSET'=> 'utf8', // 字符集
+);
---
+++ Application/Index/Controller/IndexController.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Index\Controller;
+use Think\Controller;
+class IndexController extends Controller {
+    public function index(){
+        echo 'index';
+    }
+    public function add(){
+        echo 1;
+    }
+}
