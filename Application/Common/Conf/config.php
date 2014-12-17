0c193549c7c903d308f214640281675fdafbdc62
a <1052746142@qq.com>
2014-12-17 17:07:58 +0800

    asd

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -9,15 +9,15 @@ return array(
 
      //'配置项'=>'配置值'
      'URL_CASE INSENSITIVE' =>true,
-     "DB_DEPLOY_TYPE"=>1， //是否启用分布式
+     "DB_DEPLOY_TYPE"=>1, //是否启用分布式
      'DB_RW_SEPARATE'=>true, //是否启用智能读写分离
      'DB_TYPE' => 'mysql', //数据库类型
      'DB_HOST' => '192.168.1.110,192.168.1.98', //服务器地址
-     'DB_NAME' => 'mybbs'， //数据库名
+     'DB_NAME' => 'mybbs', //数据库名
      'DB_USER' => 'root,root', //用户名
      'DB_PWD' =>  'root,root', //密码
      'DB_PREFIX' => 'bbs_', //数据库表前缀
-     "project_name"=>"bbs"，
+     "project_name"=>"bbs",
      'TMPL_L_DELIM' => '<!--{',
      'TMPL_R_DELIM' => '}-->',
      'LAYOUT_ON'=>true,
