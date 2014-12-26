fd05d26a14b9281526a5ccdfee120040e0ab06a7
weike <601462047@qq.com>
2014-12-26 08:08:24 +0800

    jj

--- Application/Runtime/Cache/Index/5f343310d4a83e1c2f6d4e5a6181e90a.php
+++ Application/Runtime/Cache/Index/5f343310d4a83e1c2f6d4e5a6181e90a.php
@@ -29,22 +29,6 @@
 	<div class="clear"></div>
 	<div class="top2">
 		<h1 class="fl"><a href="http://www.itcast.cn"><img src="/Application/Common/index/images/logo.png" alt="传智播客php培训学院" border="0"  class="png"/></a></h1>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-		<h1 class="fl"><a href="http://www.itcast.cn"><img src="/Application/Common/index/images/logo.png" alt="传智播客php培训学院" border="0"  class="png"/></a></h1>
-=======
-		<h1 class="fl"><a href="http://www.itcast.cn"><img src="/two/Application/Common/index/images/logo.png" alt="传智播客php培训学院" border="0"  class="png"/></a></h1>
->>>>>>> 3fc8412c0aa264f66c610afd7cdec40cba34737d
->>>>>>> 60d7c42115ddbed0910584fb91afd1ec00e37b49
-=======
->>>>>>> 8b388b05a480beaa3f1b26c6fc3cf1fcf705f888
->>>>>>> 8d5db131bd230081f175790322e699f970bd3725
->>>>>>> b151cee351a4e38dd1e4aa1fff5ca29e3c5e4dcb
 		<div class="fl toubu">
 		<div class="toubu-font1">PHP学院</div>
 			 <!-- 校区 -->
@@ -60,27 +44,6 @@
 		<p class="fr"><img src="http://php.itcast.cn/images/topword.gif" alt="改变中国教育，我们正在行动..." class="png"/></p>
 	</div>
 	<div class="clear"></div>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> 6599f6b847b396825cf87220a11cc29d4c42918c
->>>>>>> 8d5db131bd230081f175790322e699f970bd3725
-
->>>>>>> 3fc8412c0aa264f66c610afd7cdec40cba34737d
->>>>>>> 60d7c42115ddbed0910584fb91afd1ec00e37b49
-=======
->>>>>>> 8b388b05a480beaa3f1b26c6fc3cf1fcf705f888
->>>>>>> b151cee351a4e38dd1e4aa1fff5ca29e3c5e4dcb
 	<ul id="nav">
 	<?php foreach($a as $k=>$v){ ?>
 
@@ -181,22 +144,6 @@
 				<dl class="ganyan1 huodong1">
 	<dt class="fl">
 		<a href="http://php.itcast.cn/news/20131117/14525122556.shtml" target="_blank"><img src="/Application/Common/index/images/4.jpg" style="width: 286px; height: 215px;" /><br />
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-		<a href="http://php.itcast.cn/news/20131117/14525122556.shtml" target="_blank"><img src="/Application/Common/index/images/4.jpg" style="width: 286px; height: 215px;" /><br />
-=======
-		<a href="http://php.itcast.cn/news/20131117/14525122556.shtml" target="_blank"><img src="/two/Application/Common/index/images/4.jpg" style="width: 286px; height: 215px;" /><br />
->>>>>>> 3fc8412c0aa264f66c610afd7cdec40cba34737d
->>>>>>> 60d7c42115ddbed0910584fb91afd1ec00e37b49
-=======
->>>>>>> 8b388b05a480beaa3f1b26c6fc3cf1fcf705f888
->>>>>>> 8d5db131bd230081f175790322e699f970bd3725
->>>>>>> b151cee351a4e38dd1e4aa1fff5ca29e3c5e4dcb
 		天空一声巨响 四组闪亮登场</a></dt>
 	<dd class="huodongfr">
 		<h3 class="STYLE7">班级活动</h3>
@@ -310,22 +257,6 @@
 </ul>
 <ul>
 <li style="border-bottom: medium none; float: left; color: #515151"><a href="/Index/Index/training?id=<?php echo ($it["id"]); ?>"><b><font style="color: #515151">PHP就业班</font></b></a></li>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<li style="border-bottom: medium none; float: left; color: #515151"><a href="/Index/Index/training?id=<?php echo ($it["id"]); ?>"><b><font style="color: #515151">PHP就业班</font></b></a></li>
-=======
-<li style="border-bottom: medium none; float: left; color: #515151"><a href="/two/Index/Index/training?id=<?php echo ($it["id"]); ?>"><b><font style="color: #515151">PHP就业班</font></b></a></li>
->>>>>>> 3fc8412c0aa264f66c610afd7cdec40cba34737d
->>>>>>> 60d7c42115ddbed0910584fb91afd1ec00e37b49
-=======
->>>>>>> 8b388b05a480beaa3f1b26c6fc3cf1fcf705f888
->>>>>>> 8d5db131bd230081f175790322e699f970bd3725
->>>>>>> b151cee351a4e38dd1e4aa1fff5ca29e3c5e4dcb
 <br />
 </ul>
 <ul style="clear: both">
@@ -333,22 +264,6 @@
 </ul>
 <ul>
 <li style="border-bottom: medium none; float: left; color: #515151"><a href="/Index/Index/training"><b><font style="color: #515151">PHP远程班</font></b></a></li>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<li style="border-bottom: medium none; float: left; color: #515151"><a href="/Index/Index/training"><b><font style="color: #515151">PHP远程班</font></b></a></li>
-=======
-<li style="border-bottom: medium none; float: left; color: #515151"><a href="/two/Index/Index/training"><b><font style="color: #515151">PHP远程班</font></b></a></li>
->>>>>>> 3fc8412c0aa264f66c610afd7cdec40cba34737d
->>>>>>> 60d7c42115ddbed0910584fb91afd1ec00e37b49
-=======
->>>>>>> 8b388b05a480beaa3f1b26c6fc3cf1fcf705f888
->>>>>>> 8d5db131bd230081f175790322e699f970bd3725
->>>>>>> b151cee351a4e38dd1e4aa1fff5ca29e3c5e4dcb
 <br />
 </ul>
 <ul style="clear: both">
